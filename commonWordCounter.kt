332be1a03532b62cd384a261108d19321344a70a
Derek Briggs <contact@derekbriggs.xyz>
2020-10-06 13:42:35 -0400

    The script now reads words from an external file

--- commonWordCounter.kt
+++ commonWordCounter.kt
@@ -1,16 +1,30 @@
+import java.io.File
+
 data class Item(val word : String, val count : Int)
 
 fun main() {
 
-	val list = listOf("Spanish", "Italian", "Esperanto", "Esperanto", "German", "French")
 	val frequencyMap: MutableMap<String, Int> = HashMap()
+
+	val regex = "[^A-Za-z ]".toRegex()
+	File("wordsToCount.txt").forEachLine { 
+		val words = it.split(" ")
+		for (word in words) {
+			val wordToCount = regex.replace(word.toLowerCase(), "")
+			var count = frequencyMap[wordToCount]
+			if (count == null) count = 0
+			frequencyMap[wordToCount] = count + 1
+		}	
+	}
+
+//	val list = listOf("Spanish", "Italian", "Esperanto", "Esperanto", "German", "French")
 	
-	for (s in list) {
+/*	for (s in list) {
 		var count = frequencyMap[s]
 		if (count == null) count = 0
 		frequencyMap[s] = count + 1
 	}
-
+*/
 	val items = ArrayList<Item>()
 
 	for (s in frequencyMap) {
@@ -20,4 +34,5 @@ fun main() {
 	val sortedList = items.sortedBy { it.count }.reversed()
 
 	println(sortedList)
+	println(frequencyMap.size)
 }
