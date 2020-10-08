ab2b0ed72e23ee2cbce67226c2c1f38839f1f4ac
Derek Briggs <contact@derekbriggs.xyz>
2020-10-08 15:55:59 -0400

    break out logic into separate methods

--- commonWordCounter.kt
+++ commonWordCounter.kt
@@ -1,30 +1,13 @@
 import java.io.File
+import java.io.FileWriter
 
 data class Item(val word : String, val count : Int)
 
 fun main() {
 
-	val frequencyMap: MutableMap<String, Int> = HashMap()
+	val frequencyMap: MutableMap<String, Int> = createFrequencyMap(File("German.Words.txt"))//a file of german words with special characters to test my regex expression
 
-	val regex = "[^A-Za-z ]".toRegex()
-	File("wordsToCount.txt").forEachLine { 
-		val words = it.split(" ")
-		for (word in words) {
-			val wordToCount = regex.replace(word.toLowerCase(), "")
-			var count = frequencyMap[wordToCount]
-			if (count == null) count = 0
-			frequencyMap[wordToCount] = count + 1
-		}	
-	}
 
-//	val list = listOf("Spanish", "Italian", "Esperanto", "Esperanto", "German", "French")
-	
-/*	for (s in list) {
-		var count = frequencyMap[s]
-		if (count == null) count = 0
-		frequencyMap[s] = count + 1
-	}
-*/
 	val items = ArrayList<Item>()
 
 	for (s in frequencyMap) {
@@ -35,4 +18,31 @@ fun main() {
 
 	println(sortedList)
 	println(frequencyMap.size)
+
+	writeToFile(sortedList, frequencyMap.size)
+}
+
+fun createFrequencyMap(inputFile : File) : MutableMap<String, Int> {
+	val frequencyMap: MutableMap<String, Int> = HashMap()
+	val regex = "[^A-Za-z ]".toRegex()
+	inputFile.forEachLine { 
+		val words = it.split(" ")
+		for (word in words) {
+			val wordToCount = regex.replace(word.toLowerCase(), "")
+			var count = frequencyMap[wordToCount]
+			if (count == null) count = 0
+			frequencyMap[wordToCount] = count + 1
+		}	
+	}
+	return frequencyMap
+}
+
+fun writeToFile(list : List<Item>, individualWordCount: Int) {
+	val writer = FileWriter("Output.txt");
+	writer.write("Number Of Unique Words: $individualWordCount")
+	for (item in list) {
+		writer.write("${item.word}=${item.count}\n")
+	}
+
+	writer.close()
 }
